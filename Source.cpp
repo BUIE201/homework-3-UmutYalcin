2e1e1c558d5e29e49e0956eeccea638bc371b34d
Umut Yalçınkaya <umut.yalcinkaya@boun.edu.tr>
2021-01-25 23:46:49 +0300

    I think code works but it gives a different result than the example

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
@@ -10,7 +12,6 @@ struct Node
 
 	Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr) {}
 };
-
 void PrintTree(Node* pRoot, int Level)
 {
 	if (!pRoot)
@@ -28,23 +29,84 @@ void PrintTree(Node* pRoot, int Level)
 void InsertToTree(Node*& pRoot, Node* pNew)
 {
 	if (!pRoot)
-	{
 		pRoot = pNew;
-		return;
-	}
-
-	if (pNew->i <= pRoot->i)
-		InsertToTree(pRoot->pLeft, pNew);
 	else
-		InsertToTree(pRoot->pRight, pNew);
+	{
+		if (pNew->i < pRoot->i)
+			InsertToTree(pRoot->pLeft, pNew);
+		else
+			InsertToTree(pRoot->pRight, pNew);
+	}
 }
 
+void FindLargestSumAndBranch(Node* pRoot, int& LargestSum, vector<Node*>& LargestBranch) {
+
+	int LeftSum, RightSum = 0;
+
+	if (pRoot != nullptr) {
+
+		LargestSum = pRoot->i;
+		LargestBranch.push_back(pRoot);
+
+		if (pRoot->pLeft != nullptr && pRoot->pRight != nullptr) 
+		{
+
+			FindLargestSumAndBranch(pRoot->pLeft, LeftSum, LargestBranch);
+			FindLargestSumAndBranch(pRoot->pRight, RightSum, LargestBranch);
+
+			if (RightSum >= LeftSum) 
+			{
+				LargestSum += RightSum;
+			}
+			else 
+			{
+				LargestSum += LeftSum;
+			}
+
+		}
+		else if (pRoot->pLeft != nullptr) 
+		{
+			FindLargestSumAndBranch(pRoot->pLeft, LeftSum, LargestBranch);
+			LargestSum += LeftSum;
+		}
+		else if (pRoot->pRight != nullptr) 
+		{
+			FindLargestSumAndBranch(pRoot->pRight, RightSum, LargestBranch);
+			LargestSum += RightSum;
+		}
+	}
+}
+
+void PrintBranch(vector<Node*> branch, int sum) {
+
+	cout << "Branch with the largest sum is: ";
+
+	for (int j = 0; j < branch.size(); j++)
+	{
+		cout << branch[j]->i << " ";
+	}
+
+	cout << " -> SUM = " << sum << endl;
+
+}
+
+void PrintLargestPathAndSum(Node* pRoot) {
+
+	vector<Node*> LargestBranch;
+
+	int LargestSum = 0;
+	
+	FindLargestSumAndBranch(pRoot, LargestSum, LargestBranch);
+	PrintBranch(LargestBranch, LargestSum);
+
+}
 
 void main()
 {
 	int i;
 
 	Node* pRoot = nullptr;
+
 	while (true)
 	{
 		cin >> i;
@@ -56,4 +118,7 @@ void main()
 	}
 
 	PrintTree(pRoot, 1);
+
+	PrintLargestPathAndSum(pRoot);
+
 }
